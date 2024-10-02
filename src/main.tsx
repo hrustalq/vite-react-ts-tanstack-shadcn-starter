a4a91c0aeda66324596186c4bd20274042fbf967
hrustalq <n1k3f1t@gmail.com>
2024-10-02 13:17:59 +0800

    add react-query library for requests

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+
 import { RouterProvider } from '@tanstack/react-router'
-import "./globals.css"
 import { router } from './router'
 
-// Render the app
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+import "./globals.css"
+
+const queryClient = new QueryClient()
+
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
